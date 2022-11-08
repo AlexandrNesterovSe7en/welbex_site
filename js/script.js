2bc5319864381556c9bb8a7a90ed9820ac20ff66
SE7EN <89002828+zxcPAPASHAzxc@users.noreply.github.com>
2022-11-08 23:24:52 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,16 +9,77 @@ document.addEventListener('DOMContentLoaded', function(){
             let input = event.target;
             let inputNumberValue = input.value;
             let coretkaPos = input.selectionStart;
-            console.log(inputNumberValue)
-           
+            let foramatted;
+
             //Запрет ввода букв
-            // if (/\D/g.test(inputNumberValue)){
-            //    coretkaPos = inputNumberValue.match(/\D/).index;
-            //    input.value = inputNumberValue.replace(/\D/g, '');
-            //    input.selectionStart = input.selectionEnd = coretkaPos;
-            // }
+            if (/[^)(+_-\s\d]/g.test(inputNumberValue)){
+               --coretkaPos
+               input.value = '+' + inputNumberValue.replace(/[^)(_-\s\d]/g, '');
+               input.selectionStart = input.selectionEnd = coretkaPos;
+               return               
+            }
+
+            function getNum(foramatted, coretkaPos, indexCoretka) {
+                
+                let len = indexCoretka == 4 || indexCoretka == 9 ? 3 : 2;
+                let controlIndex = indexCoretka == 4 ? 1 : indexCoretka == 9 ? 2 : indexCoretka == 13 ? 3 : 4
+                console.log(len)
+                if (foramatted[controlIndex].length < len) {
+                    switch (coretkaPos){
+
+                        case indexCoretka:
+                            foramatted[controlIndex] = '_' + foramatted[controlIndex]
+                            break
+                        case indexCoretka + 1:
+                            foramatted[controlIndex] = foramatted[controlIndex][0] + '_' + (foramatted[controlIndex][1] ?foramatted[controlIndex][1] :'')
+                            break
+                        case indexCoretka + 2:
+                            foramatted[controlIndex] = foramatted[controlIndex] + '_'
+                            break
+                        default:
+                            foramatted = ['7', '___', '___', '__', '__']
+                    }
+                }else if (foramatted[controlIndex].length >= len){
+                    switch (coretkaPos){
+
+                        case indexCoretka + 1:
+                            foramatted[controlIndex] = foramatted[controlIndex][0] + foramatted[controlIndex].substring(2)
+                            break
+                        case indexCoretka + 2:
+                            foramatted[controlIndex] = foramatted[controlIndex].substring(0, 2) + foramatted[controlIndex].substring(3)
+                            break
+                        case indexCoretka + 3:
+                            foramatted[controlIndex] = foramatted[controlIndex].substring(0, 3) + foramatted[controlIndex].substring(4)                        
+                            break
+                        default:
+                            foramatted = ['7', '___', '___', '__', '__']
+                    }
+                }
+                console.log(foramatted, coretkaPos)
+                return `+7 (${foramatted[1]}) ${foramatted[2]}-${foramatted[3]}-${foramatted[4]}`
+            }
+            if (coretkaPos >= 4 && coretkaPos < 8) {                
+                foramatted = inputNumberValue.match(/[\d_]+/g)
+                input.value = getNum(foramatted, coretkaPos, 4)
+                input.selectionStart = input.selectionEnd = coretkaPos;
+            } else if(coretkaPos >= 9 && coretkaPos < 13){
+                foramatted = inputNumberValue.match(/[\d_]+/g)
+                input.value = getNum(foramatted, coretkaPos, 9)
+                input.selectionStart = input.selectionEnd = coretkaPos;
+            } else if(coretkaPos >= 13 && coretkaPos < 16){
+                foramatted = inputNumberValue.match(/[\d_]+/g)
+                input.value = getNum(foramatted, coretkaPos, 13)
+                input.selectionStart = input.selectionEnd = coretkaPos;
+            } else if(coretkaPos >= 16 && coretkaPos < 19){
+                foramatted = inputNumberValue.match(/[\d_]+/g)
+                input.value = getNum(foramatted, coretkaPos, 16)
+                input.selectionStart = input.selectionEnd = coretkaPos;
+            }
+            else{
+                input.value = '+7 (___) ___-__-__'
+            }
         })
-        input.addEventListener('click', function(event){
+        input.addEventListener('focus', function(event){
             let input = event.target;
             let inputNumberValue = input.value;
             let coretkaPos = 4;
@@ -27,27 +88,6 @@ document.addEventListener('DOMContentLoaded', function(){
                 input.selectionStart = input.selectionEnd = coretkaPos;    
             }
         })
-        input.addEventListener('keydown', function(event){
-            let input = event.target
-            let key = event.code
-            let coretkaPos = input.value.match(/_/)?.index
-
-            if (/\d/.test(key) && coretkaPos >= 4 && coretkaPos < 7) {
-                input.value = input.value.replace(/_/, key.replace(/\D/g, ''))
-                input.selectionStart = input.selectionEnd = ++coretkaPos;
-            }
-            if (/\d/.test(key) && coretkaPos >= 9 && coretkaPos < 12) {
-                input.value = input.value.replace(/_/, key.replace(/\D/g, ''))
-                input.selectionStart = input.selectionEnd = ++coretkaPos;
-            }
-            if (/\d/.test(key) && coretkaPos >= 13 && coretkaPos < 15) {
-                input.value = input.value.replace(/_/, key.replace(/\D/g, ''))
-                input.selectionStart = input.selectionEnd = ++coretkaPos;
-            }
-            if (/\d/.test(key) && coretkaPos >= 16 && coretkaPos < 18) {
-                input.value = input.value.replace(/_/, key.replace(/\D/g, ''))
-                input.selectionStart = input.selectionEnd = ++coretkaPos;
-            }
-        })
     })
 })
+
